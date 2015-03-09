0c5210c699ad5662e5db69e6df7524679fd25662
yargoar <yargo.ar@gmail.com>
2015-03-08 21:48:04 -0400

    V1
    Nomes aos botões.
    Atualizando e tocando automaticamente a lista.

--- GMP20/js/main.js
+++ GMP20/js/main.js
@@ -21,10 +21,7 @@ var l = require('logger'); // logger
 config.on('load-complete',function(){ l.log("Config carregado com sucesso"); });
 
 
-mManager.on('load-list', function (){
-	console.log("mManager - load-list");
-	//sConnector.loadList();
-});
+
 sConnector.on('loadedlist',function( data ){ 
 	mManager.loadList( data );
 	console.log("Lista carregada com sucesso pelo sConnector e passada ao mManager");
@@ -32,8 +29,16 @@ sConnector.on('loadedlist',function( data ){
 mManager.on('loadmusic-start',function(music){ l.log('"'+music.name+'"',"carregando.");});
 mManager.on('loadlist-complete',function(music){ l.log('"'+music.name+'"',"carregada com sucesso.");});
 mManager.on('loadmusic-complete',function(music){ l.log('"'+music.name+'"',"carregada com sucesso.");});
-mManager.on('download-completed',function(data){ l.log("Todas as músicas da nova playlist já estão carregadas."); });
-
+mManager.on('download-completed',function(data){ 
+	l.log("Todas as músicas da nova playlist já estão carregadas."); 
+	mManager.makeUpdateAvaiable();
+});
+mManager.on('autoupdate',function(data){ 
+	mPlayer.play();
+});
+mManager.on('start',function(data){ 
+	mPlayer.play();
+});
 
 // TRAY
 var tray = new gui.Tray({ title: 'GRAVE PLAYER', icon: 'images/logo_16.png' });
@@ -81,21 +86,23 @@ $(function(){
 	mManager.on('next-music',function (){ var music = mManager.nextMusic(); mPlayer.nextMusic(music); });
 	//mPlayer.on('next-music',function (){ console.log("Evento no main"); });
 	
-	
-	mManager.localList( function(){ mPlayer.init();} );
+	mManager.updateLocalList( function(){ 
+		mPlayer.init();
+		mPlayer.play();
+	} );
 
-	$('.commands input').eq(0).click(function(){
-		mManager.localList();
-		l.log("Lista de músicas atualizada.");
+	$('#bt-update').click(function(){
+		mManager.updateLocalList();
+		l.log("Atualizou lista de músicas com as musicas do servidor");
 	});
 
-	$('#playback_control > span').eq(0).click(function(){
+	$('#bt-play').click(function(){
 		mPlayer.play();
 	});
-	$('#playback_control > span').eq(1).click(function(){
+	$('#bt-forward').click(function(){
 		mPlayer.getNextMusic();
 	});
-	$('#teste').click(function(){
+	$('#bt-teste').click(function(){
 		console.log('teste');
 		mPlayer.playExtra('chamada.ogg');
 	});
